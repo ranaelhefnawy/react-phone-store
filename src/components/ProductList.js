ee4506b8df39eabe0f5fa9a7b7f8b90eb5abec25
Rana <ranaelhefnawy9@gmail.com>
2020-06-06 19:50:42 +0900

    first commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Product from './Product';
+import Title from './Title';
+import {ProductConsumer} from '../context';
+
+class ProductList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="conatiner">
+                        <Title name="our" title="Products" />
+                        <div className="row">
+                        <ProductConsumer>
+                            {value => {
+                                return value.products.map(product => {
+                                    return <Product
+                                        key={product.id}
+                                        product={product} />;
+                                })
+                            }}
+                        </ProductConsumer>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ProductList;
